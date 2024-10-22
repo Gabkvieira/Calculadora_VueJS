086b1a784aad5c3fa27ca69cdbf821cd0ebdd02c
Gabriel Vieira <gabkv99@hotmail.com>
2024-10-22 11:18:45 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<script setup>
+  import { reactive } from 'vue';
+  import Formulario from './components/Formulario.vue';
+
+  const estado = reactive({
+    primeiroNumero: 0,
+    segundoNumero: 0,
+    filtro: 'adicao',
+  })
+
+  const adicao = ({ primeiroNumero, segundoNumero } = estado) => {
+  return primeiroNumero + segundoNumero;
+}
+
+const subtracao = ({ primeiroNumero, segundoNumero } = estado) => {
+  return primeiroNumero - segundoNumero;
+}
+
+const multiplicacao = ({ primeiroNumero, segundoNumero } = estado) => {
+  return primeiroNumero * segundoNumero;
+}
+
+const divisao = ({ primeiroNumero, segundoNumero } = estado) => {
+  return primeiroNumero / segundoNumero;
+}
+
+const resultado = () => {
+  const {filtro} = estado;
+
+    switch (filtro) {
+      case 'subtracao':
+        return subtracao();
+      case 'multiplicacao':
+        return multiplicacao();
+      case 'divisao':
+        return divisao();
+      default:
+        return adicao();    
+    }
+  }
+
+</script>
+
+<template>
+  <div class="container">
+    <header class="p-5 mb-4 mt-4 text-center">
+        <h1>Calculadora:</h1>
+    </header>
+    <Formulario
+      :trocar-filtro="(event) => (estado.filtro = event.target.value)"
+      :numero1="(event) => (estado.primeiroNumero = Number(event.target.value))"
+      :numero2="(event) => (estado.segundoNumero = Number(event.target.value))"
+    />
+    <br>
+    <br>
+    <h1 class="resultado text-center border border-dark rounded p-5">O resultado é: {{ resultado() }} </h1>
+  </div>
+  
+</template>
+
+<style scoped>
+.resultado {
+  max-width: 410px;
+  margin-left: 430px;
+}
+</style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,28 @@
+<script setup>
+const props = defineProps(["numero1", "numero2", "trocarFiltro"]);
+</script>
+
+<template>
+    <form>
+        <div class="row">
+            <div class="col d-flex">
+            <input @keyup="props.numero1" required type="number" placeholder="Digite o primeiro número" class="form-control border-dark">
+            <input @keyup="props.numero2" required type="number" placeholder="Digite o segundo número" class="form-control border-dark">
+            </div>
+            <div class="col-md-4">
+                <select @change="props.trocarFiltro" class="form-control border-dark">
+                    <option value="adicao">Adição</option>
+                    <option value="subtracao">Subtração</option>
+                    <option value="multiplicacao">Multiplicação</option>]
+                    <option value="divisao">Divisão</option>
+                </select>
+            </div>
+        </div>
+    </form>
+</template>
+
+<style scoped>
+form input {
+    margin-right: 16px;
+}
+</style>
