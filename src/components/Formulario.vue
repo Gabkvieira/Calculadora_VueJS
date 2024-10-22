0789b87a5f73024a9b57182a34ec09c21d7c4925
Gabriel Vieira <gabkv99@hotmail.com>
2024-10-22 11:30:11 -0300

    alterando visao celular e css form

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -9,8 +9,8 @@ const props = defineProps(["numero1", "numero2", "trocarFiltro"]);
             <input @keyup="props.numero1" required type="number" placeholder="Digite o primeiro número" class="form-control border-dark">
             <input @keyup="props.numero2" required type="number" placeholder="Digite o segundo número" class="form-control border-dark">
             </div>
-            <div class="col-md-4">
-                <select @change="props.trocarFiltro" class="form-control border-dark">
+            <div class="col-md-4 alteraCelular">
+                <select @change="props.trocarFiltro" class="alinhaTexto form-control border-dark">
                     <option value="adicao">Adição</option>
                     <option value="subtracao">Subtração</option>
                     <option value="multiplicacao">Multiplicação</option>]
@@ -24,5 +24,20 @@ const props = defineProps(["numero1", "numero2", "trocarFiltro"]);
 <style scoped>
 form input {
     margin-right: 16px;
+    text-align: center;
+}
+
+.alinhaTexto {
+    text-align: center;
+}
+
+@media only screen and (max-width: 767px) {
+    .alteraCelular {
+        margin-top: 16px;
+    }
+
+    form input {
+    margin-right: 3px;
+}
 }
 </style>
